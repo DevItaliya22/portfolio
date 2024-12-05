f21971266abe3be3b8a5e9fd22523a81f4483bed
Dev <devitaliya22@gmail.com>
2024-12-05 19:52:47 +0530

    less loader

--- app/components/ParticleEffect.tsx
+++ app/components/ParticleEffect.tsx
@@ -27,7 +27,7 @@ const ParticleEffect = () => {
       speedY: number;
     }[] = [];
 
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 50; i++) {
       particles.push({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
