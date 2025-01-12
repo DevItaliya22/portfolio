c8b06238ab1dadf60e192260abf13b367edad3d7
Dev <devitaliya22@gmail.com>
2025-01-12 21:08:38 +0530

    Analtics

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,18 +32,19 @@ export default function RootLayout({
           type="image/x-icon"
           style={{ borderRadius: '50%' }}
         />
+        <script
+        defer
+        data-website-id="6783df6488b0ca8b0194a39b"
+        data-domain="www.devitaliya.me"
+        src="https://datafa.st/js/script.js"
+      ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
-      <script
-        defer
-        data-website-id="6783df6488b0ca8b0194a39b"
-        data-domain="www.devitaliya.me"
-        src="https://datafa.st/js/script.js"
-      ></script>
+      
     </html>
   );
 }
